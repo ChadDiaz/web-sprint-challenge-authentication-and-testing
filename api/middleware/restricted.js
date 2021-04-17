d39a0f20661105a15b25c6d316d30e719bd87d4e
ChadDiaz <chadcdiaz@gmail.com>
2021-04-16 22:24:56 -0400

    initialzed the restricted middleware

--- api/middleware/restricted.js
+++ api/middleware/restricted.js
@@ -1,6 +1,23 @@
+const jwt = require("jsonwebtoken");
+const { JWT_SECRET } = require("../config/secrets.js");
+
 module.exports = (req, res, next) => {
-  next();
-  /*
+  const token = req.headers?.authorization?.split(" ")[1];
+  if (token) {
+    jwt.verify(token, JWT_SECRET, (err, decodedToken) => {
+      if (err) {
+        res.status(401).json({ message: "token invalid" });
+      } else {
+        req.decodedToken = decodedToken;
+        next();
+      }
+    });
+  } else {
+    res.status(401).json({ message: "token required" });
+  }
+};
+
+/*
     IMPLEMENT
 
     1- On valid token in the Authorization header, call next.
@@ -11,4 +28,3 @@ module.exports = (req, res, next) => {
     3- On invalid or expired token in the Authorization header,
       the response body should include a string exactly as follows: "token invalid".
   */
-};
