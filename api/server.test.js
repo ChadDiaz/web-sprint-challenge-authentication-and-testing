6c8dd86c3c2baeb8ff91f33ec14ea928a273c53a
ChadDiaz <chadcdiaz@gmail.com>
2021-04-18 16:47:12 -0400

    mvp i think

--- api/server.test.js
+++ api/server.test.js
@@ -31,7 +31,30 @@ describe("2 test per endpoint: /api/jokes, /api/auth/login, /api/auth/register",
   });
   //
   //
-  describe('2 tests for api/auth/login', () => {
-    
-  })
+  describe("2 tests for api/auth/register", () => {
+    let body;
+    const creds = { username: "test", password: "test" };
+
+    test("receives 201 response", async () => {
+      body = await request(server).post("/api/auth/register").send(creds);
+      expect(body.status).toBe(201);
+    });
+    test("body.body.username should be 'test' ", async () => {
+      expect(body.body.username).toEqual("test");
+    });
+  });
+
+  describe("2 tests for api/auth/login", () => {
+    let body2;
+    const creds2 = { username: "test", password: "test" };
+
+    test("status to be 200", async () => {
+      await request(server).post("/api/auth/register").send(creds2);
+      body2 = await request(server).post("/api/auth/login").send(creds2);
+      expect(body2.status).toBe(200);
+    });
+    test("body2.message should equal 'welcome test' ", async () => {
+      expect(body2.body.message).toEqual("welcome test");
+    });
+  });
 });
